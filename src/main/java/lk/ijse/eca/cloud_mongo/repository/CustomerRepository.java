ea2d3925f402725128dc3c50b265fb0040cfcf3c
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2026-03-29 22:30:34 +0530

    Locally db done

--- src/main/java/lk/ijse/eca/cloud_mongo/repository/CustomerRepository.java
+++ src/main/java/lk/ijse/eca/cloud_mongo/repository/CustomerRepository.java
@@ -2,7 +2,6 @@ package lk.ijse.eca.cloud_mongo.repository;
 
 import lk.ijse.eca.cloud_mongo.entity.Customer;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 public interface CustomerRepository extends MongoRepository<Customer, String> {
 }
